12f64858fb661e6d7a6e3a667615184433a29fc7
shoonia <zaytsev126@gmail.com>
2020-02-04 22:50:47 +0200

    No use deprecated Nodejs APIs

--- src/config.js
+++ src/config.js
@@ -1,22 +1,24 @@
-const util = require('util');
-const fs = require('fs');
-const path = require('path');
+const { existsSync, readFileSync } = require('fs');
+const { join } = require('path');
 const postcssrc = require('postcss-load-config');
 
 const root = process.cwd();
 
+const isObject = (value) => value !== null && typeof value === 'object';
+const isBoolean = (value) => typeof value === 'boolean';
+
 const { minify, assetType } = (() => {
-  const parcelrc = path.join(root, '.parcelrc');
+  const parcelrc = join(root, '.parcelrc');
   const PLUGIN_NAME = 'parcel-plugin-css-to-string';
 
-  if (fs.existsSync(parcelrc)) {
+  if (existsSync(parcelrc)) {
     try {
-      const str = fs.readFileSync(parcelrc, 'utf8');
+      const str = readFileSync(parcelrc, 'utf8');
       const config = JSON.parse(str);
 
       if (
-        util.isObject(config) &&
-        util.isObject(config[PLUGIN_NAME])
+        isObject(config) &&
+        isObject(config[PLUGIN_NAME])
       ) {
         return config[PLUGIN_NAME];
       }
@@ -30,12 +32,12 @@ const { minify, assetType } = (() => {
       );
     }
   } else {
-    const pkg = path.join(root, 'package.json');
+    const pkg = join(root, 'package.json');
 
-    if (fs.existsSync(pkg)) {
+    if (existsSync(pkg)) {
       const config = require(pkg);
 
-      if (util.isObject(config[PLUGIN_NAME])) {
+      if (isObject(config[PLUGIN_NAME])) {
         return config[PLUGIN_NAME];
       }
     }
@@ -47,7 +49,7 @@ const { options, plugins } = (() => {
   try {
     return postcssrc.sync();
   } catch (error) {
-    const isMinify = util.isBoolean(minify) ? minify : true;
+    const isMinify = isBoolean(minify) ? minify : true;
     const isProd = process.env.NODE_ENV === 'production';
 
     return {
